2ea64500bc22ba7ecc5b4f7c37485f9c667e1d83
myko <awesomeme155@gmail.com>
2021-07-26 22:22:11 +0300

    feat: parse JSON responses
    - add JSON response helpers
    - use JSON response helpers with http request handlers

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(wr http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(wr, "status: available")
-	fmt.Fprintf(wr, "environment: %s\n", app.config.env)
-	fmt.Fprintf(wr, "version: %s\n", version)
-}
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version": version,
+		},
+	}
 
+	err := app.writeJSON(wr, http.StatusOK, env, nil)
+	if err != nil {
+		app.serverErrorResponse(wr, r, err)
+
+		return
+	}
+}
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,13 +9,34 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]interface{}
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1{
+	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(wr http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		wr.Header()[key] = value
+	}
+
+	wr.Header().Set("Content-Type", "application/json")
+	wr.WriteHeader(status)
+	wr.Write(js)
+
+	return nil
 }
