fdb779297c960410991dd2eba7809f0da70239b3
myko <awesomeme155@gmail.com>
2021-07-28 18:40:02 +0300

    feat: decode JSON
    - add unmarshalJSON method to the Runtime type to allow decoding of the runtime movie type field

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,10 +10,10 @@ import (
 
 func (app *application) createMovieHandler(wr http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(wr, r, &input)
