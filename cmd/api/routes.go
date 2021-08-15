19250b9f08ce609f23de8d2673151461117d0e70
myko <awesomeme155@gmail.com>
2021-08-16 01:59:01 +0300

    feat: require user activation
    - add requiresActivatedUser() middleware
    - wrap all /v1/movies/** routes inside the requiresActivatedUser() middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,12 +14,12 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.replaceMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requiresActivatedUser(app.createMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.requiresActivatedUser(app.listMoviesHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requiresActivatedUser(app.showMovieHandler))
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.requiresActivatedUser(app.replaceMovieHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.requiresActivatedUser(app.updateMovieHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requiresActivatedUser(app.deleteMovieHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.RegisterUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
